f53d5d47ea6f399b9c43e7a63b2cb934270c5895
tmwalo <tmwalo@student.wethinkcode.co.za>
2018-06-01 14:10:20 +0200

    rename operations and add test

--- tests/operations.py
+++ tests/operations.py
@@ -1,13 +1,13 @@
 class Operations:
 
-	def negate(self, fact_a):
+	def negate_op(self, fact_a):
 		return (not fact_a)
 
-	def and(self, fact_a, fact_b):
+	def and_op(self, fact_a, fact_b):
 		return fact_a and fact_b
 
-	def or(self, fact_a, fact_b):
+	def or_op(self, fact_a, fact_b):
 		return fact_a or fact_b
 
-	def xor(self, fact_a, fact_b):
+	def xor_op(self, fact_a, fact_b):
 		return fact_a ^ fact_b
---
+++ tests/operations_test.py
@@ -0,0 +1,56 @@
+from operations import Operations
+
+ops = Operations()
+count = 1
+a = True
+b = True
+c = False
+d = False
+
+if ops.negate_op(c):
+	print("{}. PASS".format(count))
+else:
+	print("{}. FAIL".format(count))
+count += 1
+
+if not ops.negate_op(a):
+	print("{}. PASS".format(count))
+else:
+	print("{}. FAIL".format(count))
+count += 1
+
+if ops.and_op(a, b):
+	print("{}. PASS".format(count))
+else:
+	print("{}. FAIL".format(count))
+count += 1
+
+if not ops.and_op(a, c):
+	print("{}. PASS".format(count))
+else:
+	print("{}. FAIL".format(count))
+count += 1
+
+if ops.or_op(d, b):
+	print("{}. PASS".format(count))
+else:
+	print("{}. FAIL".format(count))
+count += 1
+
+if not ops.or_op(c, d):
+	print("{}. PASS".format(count))
+else:
+	print("{}. FAIL".format(count))
+count += 1
+
+if ops.xor_op(c, a):
+	print("{}. PASS".format(count))
+else:
+	print("{}. FAIL".format(count))
+count += 1
+
+if not ops.xor_op(a, b):
+	print("{}. PASS".format(count))
+else:
+	print("{}. FAIL".format(count))
+count += 1
