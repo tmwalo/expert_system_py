7738b09e9ae447b40d9f5733f360164054c38313
tmwalo <tmwalo@student.wethinkcode.co.za>
2018-06-01 13:41:47 +0200

    add tests

---
+++ tests/facts_test.py
@@ -0,0 +1,8 @@
+from facts import Facts
+
+facts = Facts()
+print("A = {}".format(facts.atoms["A"]))
+print("Z = {}".format(facts.atoms["Z"]))
+print("V = {}".format(facts.atoms["V"]))
+print("Q = {}".format(facts.atoms["Q"]))
+print("X = {}".format(facts.atoms["X"]))
